60603cdfdd67b3612661dba560255289e75d296a
TristanTorchet <53313088+TristanTorchet@users.noreply.github.com>
2025-10-15 11:58:19 +0000

    Deploying to gh-pages from @ TristanTorchet/TristanTorchet.github.io@11ec35f2984608727f250ed89a8b0e56916b7d59 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -391,16 +391,20 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-i-officialy-started-my-phd-at-eis-lab-under-the-supervision-of-prof-melika-payvand",
+          title: 'I officialy started my PhD at EIS Lab under the supervision of Prof....',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-my-first-journal-paper-has-been-published-in-nature-communications-with-my-amazing-co-first-authors-simone-d-agostino-and-filippo-moro-we-proposed-denram-an-analog-circuit-implementic-dendritic-delays-for-efficient-signal-processing",
+          title: 'My first journal paper has been published in Nature Communications. With my amazing...',
           description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-i-ll-be-presenting-denram-at-the-international-conference-on-neuromorphic-computing-and-engineering-icnce-in-june",
+          title: 'I’ll be presenting DenRAM at the International Conference on Neuromorphic Computing and Engineering...',
+          description: "",
+          section: "News",},{id: "news-i-have-been-selected-to-attend-the-famously-intense-3-week-long-telluride-neuromorphic-cognition-engineering-workshop-i-ll-be-taking-part-in-the-language-and-thought-track",
+          title: 'I have been selected to attend the famously intense 3-week long Telluride Neuromorphic...',
+          description: "",
+          section: "News",},{id: "news-our-work-with-leo-zhao-on-quantizing-state-space-models-for-the-edge-got-accepted-for-a-an-oral-presentation-at-the-acm-international-conference-on-neuromorphic-systems-icons",
+          title: 'Our work with Leo Zhao on quantizing state-space models for the edge got...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
@@ -452,35 +456,35 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%74%72%69%73%74%61%6E.%74%6F%72%63%68%65%74@%75%7A%68.%63%68", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/TristanTorchet", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/tristantorchet", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=PIABIKoF5Y8C", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/TristanTorchet", "_blank");
         },
       },{
       id: 'light-theme',
